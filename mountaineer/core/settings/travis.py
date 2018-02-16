10aeaab060b0c9ecca68deb057581da05fde0d77
Chris Haumesser <1550854+wryfi@users.noreply.github.com>
2018-02-16 06:57:39 -0800

    minor updates for travis

---
+++ mountaineer/core/settings/travis.py
@@ -0,0 +1,9 @@
+from mountaineer.core.settings.common import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mountaineer',
+    },
+}
