7ab2272aac57ddf1b7a5e6095c0cb5a6e0c2a92e
Chris Haumesser <1550854+wryfi@users.noreply.github.com>
2018-08-28 11:31:23 -0700

    move fields to mountaineer.strain

--- mountaineer/strain.py
+++ mountaineer/strain.py
@@ -4,17 +4,23 @@ not defined in OpenAPI v2 (therefore not validated natively
 by connexion) and not supported by strainer.
 """
 
-from strainer import formatters, validators, ValidationException
+from functools import partial
+
+from strainer import field, formatters, validators, ValidationException
 
 from validators.email import email as valid_email
 from validators.url import url as valid_url
 from validators.uuid import uuid as valid_uuid
 
 
+#### formatters ####
+
 @formatters.export_formatter
 def enum_formatter(value, context=None):
     return value.name
 
+
+##### validators ####
 
 @validators.export_validator
 def email_validator(value, context=None):
@@ -57,3 +63,18 @@ def url_validator(value, context=None):
         else:
             raise ValidationException('{} is not a valid URL'.format(value))
     return value
+
+
+##### fields #####
+
+email_field = partial(field, validators=[email_validator()])
+
+# when using an enum_field, you will *also* need to pass in
+# a validator pointing to the relevant Enum to validate against.
+enum_field = partial(field, formatters=[enum_formatter()])
+
+url_field = partial(field, validators=[url_validator()])
+
+uuid_field = partial(field, validators=[uuid_validator()])
+
+
