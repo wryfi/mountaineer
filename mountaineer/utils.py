7a48603b4bfbdd0bcf774e23ee4c42d0fc9edd77
Chris Haumesser <1550854+wryfi@users.noreply.github.com>
2018-09-05 11:19:56 -0700

    add mountaineer.utils.print_spec function to print yaml version of assembled openapi spec

--- mountaineer/utils.py
+++ mountaineer/utils.py
@@ -1,9 +1,21 @@
+import os
+
 from flask import abort
 from sqlalchemy.orm import exc
-
 from validators.uuid import uuid as valid_uuid
+import yaml
 
-from mountaineer.app import db
+from mountaineer.app import assemble_api, create_app, db, here
+
+
+def print_spec():
+    mntnr = create_app()
+    api_version = mntnr.app.config['API_VERSION']
+    base_specfile = os.path.join(here, 'api_{}'.format(api_version), 'openapi.yml')
+    with open(base_specfile, 'rb') as basefo:
+        base_spec = yaml.safe_load(basefo.read())
+    spec = assemble_api(mntnr.app.config['MOUNTAINEER_APIS'], base_spec)
+    print(yaml.safe_dump(spec, default_flow_style=False))
 
 
 def get_object_or_404(model, *criterion):
